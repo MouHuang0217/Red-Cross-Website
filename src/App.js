ba2477f70c3c24f3e8a53af0c257b2fbc7237600
Jamil Khan <jamil924@yahoo.com>
2021-10-12 17:06:18 -0700

    Added route to About page from App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {About} from './components/About'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="/about"
           target="_blank"
           rel="noopener noreferrer"
         >
