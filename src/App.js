daf3608f37f06d61abbd2860900e45655e7498d3
Mou Huang <ml.huangsd@gmail.com>
2021-10-12 20:13:26 -0700

    firebase test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from "react";
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore'
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users")
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+    getUsers()
+    console.log(users)
+  }, [])
   return (
-    <div className="App">
+
+    < div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,8 +31,16 @@ function App() {
         >
           Learn React-Mou
         </a>
+        <div>
+          {users.map((user) => {
+            return <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Name: {user.age}</h1>
+            </div>
+          })}
+        </div>
       </header>
-    </div>
+    </div >
   );
 }
 
