c66380bb759b01736d40f437f45f90c42ab67c71
Mou Lue Huang <31867712+MouHuang0217@users.noreply.github.com>
2021-11-02 03:19:41 -0700

    Edit Profile Page prototype completed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect, } from 'react'
 import { Card, Form, Button, Container, Alert } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Logo from "../arc_logo.png";
@@ -6,10 +6,82 @@ import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 import { EditText, EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
+import { fs } from "../firebase"
 
 export default () => {
     //current user info
     const { logout, currentUser } = useAuth()
+    const [firstName, setfirstName] = useState();
+    const [lastName, setlastName] = useState();
+    const [nickname, setnickname] = useState();
+    const [documentID, setdocumentID] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        // fs.collection("posts").where("isEvent", "==", true).get()
+        getUserData();
+    })
+
+    async function getUserData() {
+        console.log(currentUser.uid);
+        if (isLoading) {
+            fs.collection("users").get()
+                .then(function (querySnapshot) {
+                    querySnapshot.forEach(function (doc) {
+                        if (currentUser.uid === doc.data().uid) {
+                            setfirstName(doc.data().firstName);
+                            console.log(doc.data().firstName);
+                            setnickname(doc.data().nickname);
+
+                            console.log(doc.data().nickname);
+
+                            console.log("docId", doc.id);
+                            setdocumentID(doc.id);
+                        }
+                    })
+                })
+        }
+        console.log("done");
+
+    }
+    const onNameChange = (event) => {
+        setIsLoading(false);
+        console.log(event);
+        setfirstName(event);
+    };
+
+    const onNickNameChange = (event) => {
+        setIsLoading(false);
+        console.log(event);
+        setnickname(event);
+    };
+
+
+    async function updateUserData() {
+        console.log("docID", documentID);
+        console.log(firstName);
+        const userDetails = {
+            firstName: firstName,
+            // lastName: lastName,
+            email: currentUser.email,
+            nickname: nickname,
+            uid: currentUser.uid,
+            isAdmin: false
+        }
+        try {
+            // setError("")
+            console.log(userDetails)
+            const collection = fs.collection("users").doc(documentID)
+            collection.update(userDetails);
+        }
+        catch (error) {
+            console.log(error);
+        }
+        setIsLoading(true);
+        // setfirstName("doc.data().firstName");
+    }
+
     return (
         <React.Fragment>
             <center>
@@ -25,8 +97,9 @@ export default () => {
                                 <div class="card-block text-center text-white">
                                     <i class="fas fa-user-tie fa-7x mt-5"></i>
 
-                                    <EditText name="Name" type="name" style={{ width: '200px' }} defaultValue="First Name Last Name" inline />
-                                    <h6><EditText name="Bio" type="Bio" style={{ width: '200px' }} defaultValue="Bio" inline /></h6>
+                                    <EditText readonly="true" name="Name" type="name" style={{ width: '200px' }} defaultValue="First Name Last Name" inline />
+                                    <h6><EditText name="firstName" type="text" style={{ width: '200px' }} defaultValue={"n/a"} value={firstName} onChange={onNameChange} onSave={updateUserData} inline /></h6>
+                                    <h6><EditText name="Bio" type="Bio" style={{ width: '200px' }} defaultValue={"Bio"} inline /></h6>
                                     <i class="far fa-edit fa-2x mb-4"></i>
                                 </div>
                             </div>
@@ -41,14 +114,14 @@ export default () => {
                                     </div>
                                     <div class="col-sm-6">
                                         <p class="font-weight-bold">Nickname:</p>
-                                        <h6 class="text-muted"><EditText name="Nickname" type="nickname" style={{ width: '200px' }} defaultValue="Nickname" inline /></h6>
+                                        <h6 class="text-muted"><EditText name="Nickname" type="nickname" defaultValue="n/a" value={nickname} style={{ width: '200px' }} defaultValue="Nickname" onSave={updateUserData} onChange={onNickNameChange} inline /></h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        {/* <Button href="/EditProfile">Edit</Button> */}
                     </div>
                 </div>
-
             </body>
         </React.Fragment>
     )
