da922cfc3dfd2f8fc2153f3e91c1daeb31f8e0bf
Mou Lue Huang <31867712+MouHuang0217@users.noreply.github.com>
2021-11-04 13:26:03 -0700

    Added flow of hrefs

--- src/components/ListEmails.js
+++ src/components/ListEmails.js
@@ -1,8 +1,8 @@
 import { database } from 'firebase'
 import React from 'react'
 import firebase from "../firebase"
-import {Table} from 'react-bootstrap'
-
+import { Table } from 'react-bootstrap'
+import AdminNavigation from './AdminNavagationBar';
 
 function App() {
   const [spells, setSpells] = React.useState([])
@@ -17,31 +17,34 @@ function App() {
   }, [])
 
   return (
-    
-    <Table striped bordered hover>
-      
-  <thead>
-    <div>
-      <h1>ListofEmails</h1>
-    </div>
-    <tr>
-      {/* <th>#</th> */}
-      <th>First Name</th>
-      <th>Email</th>
-      {/* <th>Username</th> */}
-    </tr>
-  </thead>
-  <tbody>
-  {spells.map(spell => (
-        
-        <tr key={spell.uid}>
-               <td>{spell.firstName}</td>
-               <td>{spell.email}</td>
+    <div className="formbody">
+      <AdminNavigation /> {/*calling the navigation component to display*/}
+      <Table striped bordered hover>
+
+        <thead>
+
+          <div>
+            <h1>ListofEmails</h1>
+          </div>
+          <tr>
+            {/* <th>#</th> */}
+            <th>First Name</th>
+            <th>Email</th>
+            {/* <th>Username</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {spells.map(spell => (
+
+            <tr key={spell.uid}>
+              <td>{spell.firstName}</td>
+              <td>{spell.email}</td>
             </tr>
-      ))}
-  </tbody>
-</Table>
-  
+          ))}
+        </tbody>
+      </Table>
+    </div>
+
   );
 }
 export default App;
@@ -84,7 +87,7 @@ export default App;
 //           ...doc.data(),
 //           key: doc.id,
 //         });
-        
+
 //       });
 //       setPosts(getPostsFromFirebase);
 //       setLoading(false)
@@ -293,38 +296,38 @@ export default App;
 // import { fs } from "../firebase"
 // // import { useState } from 'react';
 // import './read.css';
-  
+
 // const Read = () => {
-  
+
 //     const [info , setInfo] = useState([]);
-  
+
 //     // Start the fetch operation as soon as
 //     // the page loads
 //     window.addEventListener('load', () => {
 //         Fetchdata();
 //       });
-  
+
 //     // Fetch the required data using the get() method
 //     const Fetchdata = ()=>{
 //         fs.collection("data").get().then((querySnapshot) => {
-             
+
 //             // Loop through the data and store
 //             // it in array to display
 //             querySnapshot.forEach(element => {
 //                 var data = element.data();
 //                 setInfo(arr => [...arr , data]);
-                  
+
 //             });
 //         })
 //     }
-      
+
 //     // Display the result on the page
 //     return (
 //         <div>
 //             <center>
 //             <h2>Student Details</h2>
 //             </center>
-          
+
 //         {
 //             info.map((data) => (
 //               course = {data.courseEnrolled}
@@ -334,30 +337,30 @@ export default App;
 //             ))
 //         }
 //         </div>
-  
+
 //     );
 // }
-  
+
 // // Define how each display entry will be structured
 // const Frame = ({course , name , age}) => {
 //     console.log(course + " " + name + " " + age);
 //     return (
 //         <center>
 //             <div className="div">
-                  
+
 // <p>NAME : {name}</p>
-   
-                  
+
+
 // <p>Age : {age}</p>
-  
-                  
+
+
 // <p>Course : {course}</p>
-   
+
 //             </div>
 //         </center>
 //     );
 // }
-  
+
 // export default Read;
 
 
@@ -477,7 +480,7 @@ export default App;
 //   var data;
 //   // const arr = [];
 //   const [info , setInfo] = useState([]);
-  
+
 //   // // Start the fetch operation as soon as
 //   // // the page loads
 //   // window.addEventListener('load', () => {
@@ -496,18 +499,18 @@ export default App;
 //           //     const listItems = numbers.map((data) =>
 //           //       <li>{number}</li>
 //           //     );
-                
+
 //           // });
 //           //doc.data() is never undefined for query doc snapshots
 //         const documents = querySnapshot.docs.map(doc => doc.data())
 //         var list = documents.createElement("ul");
-        
-        
+
+
 //       });
 
 
 //   // }
-    
+
 //   // Display the result on the page
 //   return (
 //       <div>
@@ -515,7 +518,7 @@ export default App;
 //           <h2>Student Details</h2>
 //           {/* <h1>{arr.map()}</h1> */}
 //           </center>
-        
+
 //       {
 //           // info.map((data) => (
 //           // <Frame email={data.email} 
@@ -535,15 +538,15 @@ export default App;
 // //   return (
 // //       <center>
 // //           <div className="div">
-                
+
 // // <p>NAME : {name}</p>
- 
-                
+
+
 // // {/* <p>Age : {age}</p> */}
 
-                
+
 // // <p>Email : {email}</p>
- 
+
 // //           </div>
 // //       </center>
 // //   );
